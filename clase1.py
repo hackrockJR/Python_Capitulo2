c4001a08be320ad94b613622fc566521444ea840
hackrockJR <jfromero160@hotmail.com>
2020-02-19 16:39:24 -0600

    Primerdia

--- clase1.py
+++ clase1.py
@@ -2,3 +2,115 @@
 
 # Clase 1 de Operadores y Entrada y salida de datos. 
 
+# Operadores 
+
+# Aritmeticos + - * / % ** //
+
+# Asignacion = 
+
+likes = 4
+
+print (likes)
+
+likes = likes + 1
+
+print (likes)
+
+likes += 1
+
+print (likes)
+
+Total = 0 
+Likes = 4 
+retweets = 5 
+
+Total = Likes + retweets
+
+print (Total)
+
+# Operador comparacion == != <>
+
+Resultado1 = Likes == retweets
+Resultado2 = Likes != retweets
+
+print (Resultado1)
+print (Resultado2)
+
+Likes == 4
+
+Likes == "Hola"
+
+likes > retweets
+retweets > likes
+
+likes >= retweets
+likes <= retweets
+
+# Operador pertenencia , regresa valor True or False 
+
+4 in [4,5,6,7]
+3 in [4,5,6,7]
+3 not in [4,5,6,7]
+
+
+# Operador identidad is , is not
+
+apellidos = none
+apellidos is none 
+apellidos is not none 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
