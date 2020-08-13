8f654c9d89e28646aeea59f22dc843423b23d638
Juan Romero <jfromero160@hotmail.com>
2020-08-13 14:37:16 -0600

    python3
    Estudio de Agosto 2020

---
+++ miscelaneos.py
@@ -0,0 +1,27 @@
+
+a= int(input("Digite el valod de a= \n"))
+b= int(input("Digite el valor de b= \n"))
+c= int(input("Digite el valor de c= \n"))
+
+
+
+# Continuacion de linea \
+total= a + \
+       b + \
+       c
+
+print(total)
+
+# En el caso de declaraciones contenidas no es necesario el uso de \
+
+dias = ['Lunes','Martes','Miercoles','Jueves',
+'Viernes']
+
+print(dias)
+
+# Uso de comillas
+
+Palabra = 'Esta es una palabra' 
+Oracion = "Esta es una oracion" 
+parrafo = """Este parrafo esta en multiples 
+lineas"""
